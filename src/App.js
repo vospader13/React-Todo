d3f3ead13a3323585a11d534d734febcb1c90179
vospader13 <justenmcdonald@gmail.com>
2020-02-11 19:35:53 -0700

    set up project and built out app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class App extends React.Component {
+
+
+const tasks = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+];
+
+class App extends Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  //Constructor set up
+  constructor() {
+    super();
+    this.state = {
+      tasks
+    };
+  }
+  // add new item
+  addItem = (e, item) => {
+    e.preventDefault();
+
+    const newItem = {
+      name: item,
+      id: Date.now(),
+      completed: false
+    };
+
+    this.setState({
+      completed: [...this.state.completed, newItem]
+    });
+  };
+
+  toggleItem = itemId => {
+    
+    this.setState({
+      completed: this.state.completed.map(item => {
+        console.log(item);
+        if (itemId === item.id) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        }
+
+        return item;
+      })
+    });
+  };
+
+  clearCompleted = e => {
+    e.preventDefault();
+    console.log(this.state.groceries);
+    this.setState({
+      completed: this.state.completed.filter(item => item.completed === false)
+    });
+  };
+
+
+
   render() {
     return (
       <div>
